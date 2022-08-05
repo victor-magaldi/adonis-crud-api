4b30e16fb2ac5a6d6ea962ec3eb105768f7952c7
victor de souza magaldi <victmagaldi@hotmail.com>
2022-08-04 21:01:00 -0300

    create controller post

---
+++ Controllers/Http/PostsController.ts
@@ -0,0 +1,59 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class PostsController {
+  public async index({ request, response }: HttpContextContract) {
+    console.log(request, response)
+    return [
+      {
+        id: 1,
+        title: 'Hello world',
+      },
+      {
+        id: 2,
+        title: 'Hello universe',
+      },
+    ]
+  }
+  public async store({ request, response }: HttpContextContract) {
+    console.log(request, response)
+
+    return [
+      {
+        id: 1,
+        title: 'Hello world',
+      },
+      {
+        id: 2,
+        title: 'Hello universe',
+      },
+    ]
+  }
+  public async update({ request, response }: HttpContextContract) {
+    console.log(request, response)
+
+    return [
+      {
+        id: 1,
+        title: 'Hello world',
+      },
+      {
+        id: 2,
+        title: 'Hello universe',
+      },
+    ]
+  }
+  public async destroy({ request, response }: HttpContextContract) {
+    console.log(request, response)
+
+    return [
+      {
+        id: 1,
+        title: 'Hello world',
+      },
+      {
+        id: 2,
+        title: 'Hello universe',
+      },
+    ]
+  }
+}
