4b6dda7833540a9f41223181e003b8de4143718e
victor de souza magaldi <victmagaldi@hotmail.com>
2022-08-12 23:17:46 -0300

    create post

--- Controllers/Http/PostsController.ts
+++ Controllers/Http/PostsController.ts
@@ -8,18 +8,15 @@ class PostsController {
     return DATA
   }
   public async store({ request, response }: HttpContextContract) {
-    console.log(request, response)
-
-    return [
-      {
-        id: 1,
-        title: 'Hello world',
-      },
-      {
-        id: 2,
-        title: 'Hello universe',
-      },
-    ]
+    const { title, userId, body } = request.body()
+    const newPost = {
+      userId,
+      id: DATA.length + 1,
+      title,
+      body,
+    }
+    DATA.push(newPost)
+    return response.status(200).json(newPost)
   }
   public async show({ request }: HttpContextContract) {
     const id = request.param('id')
